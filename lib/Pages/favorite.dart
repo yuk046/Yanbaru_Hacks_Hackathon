0940a2ab58d8a81426980d4eebfc2f3671829f08
oshiro <yukidora6464@gmail.com>
2024-09-07 22:31:44 +0900

    sa

--- lib/Pages/favorite.dart
+++ lib/Pages/favorite.dart
@@ -5,24 +5,29 @@ import 'package:munimuniohagi/Pages/test.dart';
 class favorite extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    // フックを使って状態を管理
     final favoriteEvents = useState<List<favoriteEvent>>([]);
-    final userId = 'exampleUserId'; // 実際のユーザー ID に置き換えてください
+    final isLoading = useState(true);
+    final userId = 'exampleUserId'; // 実際のユーザーIDに置き換えてください
 
     useEffect(() {
-      // Firestore からデータを取得
       Future<void> fetchData() async {
         try {
           final events = await getFavoritesByUserId(userId);
           favoriteEvents.value = events;
+          isLoading.value = false;
         } catch (e) {
-          print('Error fetching data: $e');
+          print('お気に入りイベントの取得中にエラーが発生しました: $e');
+          isLoading.value = false;
         }
       }
-      
+
       fetchData();
       return null; // Clean up function, not needed here
     }, []);
+
+    if (isLoading.value) {
+      return Center(child: CircularProgressIndicator());
+    }
 
     return ListView.builder(
       itemCount: favoriteEvents.value.length,
@@ -30,27 +35,27 @@ class favorite extends HookWidget {
         final event = favoriteEvents.value[index];
 
         return Container(
-          margin: EdgeInsets.all(10), // 四角形の外側の余白
+          margin: EdgeInsets.all(10),
           decoration: BoxDecoration(
-            color: Colors.white, // 背景色
-            borderRadius: BorderRadius.circular(15), // 角を丸くする
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
             boxShadow: [
               BoxShadow(
                 color: Colors.grey.withOpacity(0.5),
                 spreadRadius: 2,
                 blurRadius: 5,
-                offset: Offset(0, 3), // 影の位置
+                offset: Offset(0, 3),
               ),
             ],
-            border: Border.all(color: Color(0xffE2C6FF), width: 4), // 四角形の枠線
+            border: Border.all(color: Color(0xffE2C6FF), width: 4),
           ),
           child: Padding(
-            padding: const EdgeInsets.all(8.0), // 四角形内の余白
+            padding: const EdgeInsets.all(8.0),
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 ClipRRect(
-                  borderRadius: BorderRadius.circular(10), // 画像の角を丸くする
+                  borderRadius: BorderRadius.circular(10),
                   child: Image.network(
                     event.img,
                     width: 100,
@@ -58,7 +63,7 @@ class favorite extends HookWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
-                SizedBox(width: 10), // テキストと画像の間のスペース
+                SizedBox(width: 10),
                 Expanded(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -66,15 +71,14 @@ class favorite extends HookWidget {
                       Text(
                         event.name,
                         style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                        overflow: TextOverflow.ellipsis, // テキストが長い場合に省略表示
+                        overflow: TextOverflow.ellipsis,
                       ),
                       SizedBox(height: 5),
                       Text(
                         event.place,
                         style: TextStyle(fontSize: 16, color: Colors.grey[600]),
-                        overflow: TextOverflow.ellipsis, // テキストが長い場合に省略表示
+                        overflow: TextOverflow.ellipsis,
                       ),
-                      SizedBox(height: 5),
                     ],
                   ),
                 ),
