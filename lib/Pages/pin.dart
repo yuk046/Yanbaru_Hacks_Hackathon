bfc1c0b467575384f9d94e0b13ddb428dd472d65
oshiro <yukidora6464@gmail.com>
2024-09-07 15:27:05 +0900

    bottomNavigationBar

---
+++ lib/Pages/pin.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class BusinessPage extends StatelessWidget {
+  const BusinessPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Business Page',
+        style: Theme.of(context).textTheme.headlineMedium,
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:munimuniohagi/main.dart';
+import 'package:munimuniohagi/route.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
