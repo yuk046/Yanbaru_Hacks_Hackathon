a00870c316e61d18966b52af499bcf571d75ebf5
Yuta Matayoshi <yuuta.matayoshi1413@gmail.com>
2024-09-07 19:09:47 +0900

    Constantの作成

--- lib/Pages/start.dart
+++ lib/Pages/start.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:munimuniohagi/main.dart';
+import 'package:munimuniohagi/constant/constant.dart';
 
 class StartPage extends StatelessWidget {
   const StartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final double deviceHeight = MediaQuery.of(context).size.height;
-    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double deviceHeight = MediaQuery.of(context).size.height; //端末の縦の大きさを取得
+    final double deviceWidth = MediaQuery.of(context).size.width; //端末の横の大きさを取得
     return Scaffold(
+      backgroundColor: Color(Constant.backGroundColor),
       body: Column(
         children: [
           SizedBox(height: deviceHeight * 0.1),
           Padding(
             padding: const EdgeInsets.all(8.0),
+            // ロゴ画像
             child: Center(
               child: Container(
                 child: Image.asset('assets/images/munimuni.png'),
