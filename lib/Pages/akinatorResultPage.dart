a6ee907d895e38cdc19010ddd356cbc4e928d1f6
potetius <120088857+potetius@users.noreply.github.com>
2024-09-07 21:01:28 +0900

    resultPageを簡易的に表示

--- lib/Pages/akinatorResultPage.dart
+++ lib/Pages/akinatorResultPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:munimuniohagi/Pages/akinatorPage.dart';
+import 'package:munimuniohagi/Pages/post.dart';
 import 'package:munimuniohagi/constant/constant.dart';
 
 class AkinatorResultPage extends StatelessWidget {
@@ -6,85 +8,28 @@ class AkinatorResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
       backgroundColor: Color(Constant.mainBackGround),
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => {
+            // ボタンを押すとアキネイター画面に遷移
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const AkinatorPage()),
+            )
+          },
+          icon: const Icon(Icons.arrow_back_ios_new)
+        ),
+      ),
       body: Center(
         child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-         children: [
-         //ロゴの表示
-          Container(
-            width: screenSize.width * 0.8,
-            child: Image.asset('assets/images/munimuni.png')
-          ),
-          SizedBox(
-            height: screenSize.height * 0.08,
-          ),
-          //質問の表示 
-          Text(
-            "Q. 質問の表示",
-            style: TextStyle(
-              fontSize: 25,
-            ),
-          ),
-          SizedBox(
-            height: screenSize.height * 0.05,
-          ),
-
-          //選択肢を表示
-          Row(
-          mainAxisSize: MainAxisSize.min,
-           children: [
-            choices(screenSize: screenSize),
-            choices(screenSize: screenSize),
-           ],
-          )
+          children: [
+            Expanded(child: post()),
         ],),
       )
-    );
-  }
-}
-
-class choices extends StatelessWidget {
-  const choices({
-    super.key,
-    required this.screenSize,
-  });
-
-  final Size screenSize;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: InkWell(
-        onTap: () {
-          showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                title: Text('動作の確認'),
-              );
-            });
-        },
-        child: Container(
-          width: screenSize.width * 0.4,
-          height: screenSize.width * 0.4,
-          decoration: BoxDecoration(
-            color: Color(0xffEFE3FF),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          alignment: Alignment(0.0, 0.0),
-          child: Text(
-            "選択肢",
-            style: TextStyle(
-              fontSize: 15
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class HomePage extends HookWidget {
     final List<Widget> _pages = <Widget>[
       const AkinatorPage(),
       const BusinessPage(),
-      const UserPage()
+      const userPage()
     ];
 
     // タブが選択された時にインデックスを更新するメソッド
