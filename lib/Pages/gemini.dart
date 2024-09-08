600d3423b6f434a8dc547e9906e3319b8e8eabfc
Yuta Matayoshi <yuuta.matayoshi1413@gmail.com>
2024-09-08 09:45:49 +0900

    add env

--- lib/Pages/gemini.dart
+++ lib/Pages/gemini.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
-
-const apiKey = "AIzaSyAHtrmyvqtz0fPT6C629b94-ZLVtLtfim8";
+import 'package:munimuniohagi/env/env.dart';
 
 class Gemini {
   Future<String?> getText(String prompt) async {
     final model = GenerativeModel(
       model: 'gemini-pro',
-      apiKey: apiKey,
+      apiKey: Env.key,
     );
 
     final content = [
